78a880fcb582c298a669c1ff0bc565090b7367a8
Bruno <b.rezzio.93@gmail.com>
2019-01-24 17:34:00 -0300

    Init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+using Photon.Pun;
+using Photon.Realtime;
+
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace Com.MyCompany.MyGame
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+
+        [SerializeField]
+        private Text txt;
+        [SerializeField]
+        private Text nombreSala;
+
+        private Scene currentScene;
+        private string SceneName;
+        
+
+        private static List<string> RoomList = new List<string>();
+        ExitGames.Client.Photon.Hashtable CustomProps = new ExitGames.Client.Photon.Hashtable();
+
+        private static bool createRoom;
+
+
+        #region Photon Callbacks
+
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                //LoadArena();
+            }
+        }
+
+
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                //LoadArena();
+            }
+        }
+
+        #endregion
+
+
+
+
+
+        #region MonoBehaviour Callbacks
+        // Use this for initialization
+        void Awake()
+        {
+            //DontDestroyOnLoad(this);
+        }
+
+        void Start()
+        {
+            currentScene = SceneManager.GetActiveScene();
+            SceneName = currentScene.name;                                        
+        }
+
+        void Update()
+        {
+
+            if(SceneName == "02 Lobby")
+            {
+                if (PhotonNetwork.IsConnected) ListarSalas();
+
+            }
+            
+            if (SceneName == "05 Espera")
+            {                
+                                
+                if (PhotonNetwork.IsMasterClient) PlayersInRoom();
+                else txt.text = "Esperando Jugadores... "+ PhotonNetwork.CurrentRoom.PlayerCount+" de "+ PhotonNetwork.CurrentRoom.MaxPlayers;
+                nombreSala.text = "Sala: " + PhotonNetwork.CurrentRoom.Name;                         
+
+            }
+            
+        }
+
+        #endregion
+
+
+
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+
+        public void SwitchScenes(int idScene)
+        {
+            SceneManager.LoadScene(idScene);
+        }
+
+        /// <summary>
+        /// Se crea la sala con los parametros ingresados por el Host 
+        ///  </summary>
+        public void CrearSala()
+        {
+            Debug.Log("CrearSala()");
+            int cantidad = System.Convert.ToInt32(Parametros.param.cantidad);            
+            if (cantidad <= 20)
+            {
+                PhotonNetwork.CreateRoom(PhotonNetwork.LocalPlayer.NickName, new RoomOptions
+                {
+                    //CustomRoomProperties =  {
+                    //    {"monto",Parametros.param.monto },
+                    //    {"precio",Parametros.param.precio },
+                    //    {"ganancia",Parametros.param.ganancia },
+                    //},            
+                    MaxPlayers = System.Convert.ToByte(cantidad),
+                    IsVisible = true,
+                });
+
+                SaveRoom();
+                //SetPlayerProperties();            
+                SwitchScenes(5);
+            }
+        }
+
+        public void SetPlayerProperties()
+        {
+            ExitGames.Client.Photon.Hashtable PlayerCustomProps = new ExitGames.Client.Photon.Hashtable();
+            CustomProps["monto"] = Parametros.param.monto;
+            CustomProps["precio"] = Parametros.param.precio;
+            CustomProps["ganancia"] = Parametros.param.ganancia;
+            PhotonNetwork.SetPlayerCustomProperties(CustomProps);
+        }
+
+        /// <summary>
+        /// Se añaden los parametros de la sala al objeto Room
+        /// </summary>
+        public void SetRoomProperties()
+        {
+            CustomProps["monto"] = Parametros.param.monto;
+            CustomProps["precio"] = Parametros.param.precio;
+            CustomProps["ganancia"] = Parametros.param.ganancia;
+            Debug.Log(CustomProps["monto"]);
+            PhotonNetwork.CurrentRoom.SetCustomProperties(CustomProps);
+            
+        }
+
+
+        public void JoinRandomRoom()
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public void JoinSelectedRoom()
+        {
+            string SelectedRoom = PhotonNetwork.MasterClient.NickName;
+            PhotonNetwork.JoinRoom(SelectedRoom);
+        }
+
+
+        /// <summary>
+        /// Función utilizada para saber si el jugador creará o se unirá a una sala
+        /// </summary>
+        /// <param name="create"></param>
+        public void GoLogin(bool create)
+        {
+            createRoom = create;
+            SwitchScenes(1);
+        }
+
+        public void CreateOrJoin()
+        {
+            if (PhotonNetwork.IsConnected)
+            { 
+                PhotonNetwork.JoinLobby();
+                Debug.Log("CreateOrJoin " + createRoom);
+                if (createRoom == true) SwitchScenes(4);
+                else SwitchScenes(2);
+                //PhotonNetwork.JoinRandomRoom();
+            }
+        }
+
+
+        /// <summary>
+        /// El host actualiza los parametros de la sala y comienza el juego
+        /// </summary>
+        public void ComenzarJuego()
+        {                    
+            if (PhotonNetwork.IsMasterClient)
+            {                
+                SetRoomProperties();
+                Debug.Log("Monto (Sala): " + PhotonNetwork.CurrentRoom.CustomProperties["monto"]);
+                LoadArena();
+                
+
+            }
+        }
+
+        public void ListarSalas()
+        {
+            if (PhotonNetwork.CurrentLobby.IsDefault)
+            {
+                Debug.Log("Lobby is Default");
+                string sqlLobbyFilter = "C0 = 0";
+                PhotonNetwork.GetCustomRoomList(PhotonNetwork.CurrentLobby, sqlLobbyFilter);
+                Debug.Log(PhotonNetwork.CountOfRooms);
+                //PhotonNetwork.room
+            }
+            //LoadRoom();
+            //foreach (string str in RoomList)
+            //{
+            //    Debug.Log(str);
+            //    txt.text = str;
+            //}
+                                                
+
+        }
+
+        public void SaveRoom()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(Application.persistentDataPath +"/RoomList.dat");
+            string nombreSala = PhotonNetwork.LocalPlayer.NickName;
+            bf.Serialize(file, nombreSala);
+            file.Close();
+            
+        }
+
+        public void LoadRoom()
+        {
+            if(File.Exists(Application.persistentDataPath + "/RoomList.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();                
+                FileStream file = File.Open(Application.persistentDataPath + "/RoomList.dat", FileMode.Open);
+                string nombreSala = (string)bf.Deserialize(file);
+                RoomList.Add(nombreSala);
+                file.Close();
+            }
+        }    
+
+        #endregion
+
+
+
+
+
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.LogFormat("PhotonNetwork : Player on the Room : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            //PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("06 Sala");
+        }           
+
+        private void PlayersInRoom()
+        {
+            int i = System.Convert.ToInt32(PhotonNetwork.CurrentRoom.PlayerCount);            
+            txt.text = "Jugadores Conectados:\n"+System.Convert.ToString(PhotonNetwork.CurrentRoom.PlayerCount) 
+                                + "/" + PhotonNetwork.CurrentRoom.MaxPlayers;
+            while (i != 0) {
+                i--;
+                Debug.Log("Players in Room: "+PhotonNetwork.PlayerList[i].NickName);
+                
+            }
+        }
+
+        #endregion
+    }
+}
+
---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class Jugador : MonoBehaviourPunCallbacks
+{
+    private Image avatar;
+    private string nombre;
+    private bool[] pago = new bool[10];
+    private bool[] llega = new bool[10];
+    private int billetera;
+    [SerializeField]
+    private Text t_dias;
+
+    private int dias = 0;
+    float probabilidad;
+
+    private int evasores = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        billetera = System.Convert.ToInt32(PhotonNetwork.CurrentRoom.CustomProperties["monto"]);                
+        t_dias.text = "Día "+ System.Convert.ToString(dias+1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Se añade la selección del jugador a su historial de pago y se descuenta saldo de la billetera del jugador 
+    /// </summary>
+    public void Pagar(bool button)
+    {
+        if (button) Debug.Log("Se pagó hoy");
+        else        Debug.Log("No se pagó hoy");
+        pago[dias] = button;
+        if (pago[dias])
+            billetera = billetera-System.Convert.ToInt32(PhotonNetwork.CurrentRoom.CustomProperties["precio"]);
+        
+
+    }
+
+    public void Llegar()
+    {
+        llega[dias]=CalcularViaje();
+        if (llega[dias])
+            billetera = billetera+ System.Convert.ToInt32(PhotonNetwork.CurrentRoom.CustomProperties["ganancia"]);        
+    }
+
+
+    /// <summary>
+    /// Calcula la probabilidad de llegar al destino en base a la cantidad de evasores totales
+    /// </summary>
+    public bool CalcularViaje()
+    {
+        probabilidad=1f;
+        int i = 0, evasores = 0;
+        float x = 0f;
+        double uno = 1;
+        while (i != dias)
+        {
+            if (pago[i] == false)
+                evasores++;
+            i++;
+        }
+        //probabilidad = 1 - 1 / (1 + uno ^ (13 * (x - 0.5)));
+        //probabilidad=1/(1 ^ ( 13 *( x - 0.5 ) ) );
+        return true;
+
+    }
+
+    /// <summary>
+    /// Revisa que el jugador pague su pasaje y en base a eso calcula la probabilidad de llegar junto con los 
+    /// calculos del pasaje de bus
+    /// </summary>
+    /// <param name="button"></param>
+    public void OtroDia(bool button)
+    {        
+        if (dias < 9) {
+            Pagar(button);
+            Llegar();
+            dias++;
+            Debug.Log("Comienza Día " + (dias+1));
+            Debug.Log("Saldo "+PhotonNetwork.LocalPlayer.NickName+": "+ billetera);
+            t_dias.text = "Día " + System.Convert.ToString(dias+1);
+        }
+        else
+        {
+            Debug.Log("Finalizado");
+            PhotonNetwork.LeaveRoom();
+            SceneManager.LoadScene(0);
+        }
+    }
+}
